48b9cff781794243aadd56e5576d657598acc303
UTSgi <130817278+UTSgi@users.noreply.github.com>
2023-09-12 10:24:26 +0700

    Update login-script.js
    Mengganti format komentar sesuai dengan format JSDOC

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -4,19 +4,22 @@
  * Gunakan fungsi goToLogin() untuk menampilkan halaman login
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
-
+// mengambil nilai input dari berkas html
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+//Membuat variabel untuk menyimpan nilai email dan password supaya dapat masuk
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  // menyimpan nilai value
   const email = inputEmailElement.value;
   const password = inputPasswordElement.value;
+  //Membandingkan nilai variabel dan menentukan keputusan 
   if (email == expectedEmail && password == expectedPassword) {
     goToHome();
   } else {
